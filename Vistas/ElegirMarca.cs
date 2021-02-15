dc5d1bfd3fed4cac8b425201ba3d6a88012ec5e7
SantiagoGuirado <santiagoadrianguirado@gmail.com>
2021-02-15 11:18:36 -0300

    Se elimino variable de ElegirMarca
    Se elimino una variable booleana que no era utilizada del
    formulario ElegirMarca

--- Vistas/ElegirMarca.cs
+++ Vistas/ElegirMarca.cs
@@ -8,7 +8,6 @@ namespace AppFrancisco.Vistas
     {
         //Atributos
         private DataTable dataTable;
-        private Boolean bandera;
         private int marcaSeleccionada;
 
         //Constructor
@@ -20,7 +19,6 @@ namespace AppFrancisco.Vistas
         //Metodos
         public void cargarComboBox()
         {
-            _bandera = false;
             cbMarcasCelulares.DataSource = _dataTable;
             cbMarcasCelulares.ValueMember = "IdMarca";
             cbMarcasCelulares.DisplayMember = "NombreMarca";
@@ -33,13 +31,11 @@ namespace AppFrancisco.Vistas
 
         private void btnBuscar_Click(object sender, EventArgs e)
         {
-            _bandera = true;
             _marcaSeleccionada = Convert.ToInt32(cbMarcasCelulares.SelectedIndex.ToString()) + 1;
             this.Close();
         }
 
         //Setters && Getters
-        public bool _bandera { get => bandera; set => bandera = value; }
         public int _marcaSeleccionada { get => marcaSeleccionada; set => marcaSeleccionada = value; }
         public DataTable _dataTable { get => dataTable; set => dataTable = value; }
     }
