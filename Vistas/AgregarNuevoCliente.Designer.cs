0f2fd1e284f9946e8305f6310aa4b0c48b6bde71
SantiagoGuirado <santiagoadrianguirado@gmail.com>
2021-02-19 18:57:01 -0300

    Nuevo formulario AgregarNuevoCliente creado

---
+++ Vistas/AgregarNuevoCliente.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace AppFrancisco.Vistas
+{
+    partial class AgregarNuevoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbTittle = new System.Windows.Forms.Label();
+            this.tbDNI = new System.Windows.Forms.TextBox();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.lbDNI = new System.Windows.Forms.Label();
+            this.lbNombre = new System.Windows.Forms.Label();
+            this.lbTelefono = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbTittle
+            // 
+            this.lbTittle.AutoSize = true;
+            this.lbTittle.Font = new System.Drawing.Font("Cambria", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTittle.Location = new System.Drawing.Point(26, 19);
+            this.lbTittle.Name = "lbTittle";
+            this.lbTittle.Size = new System.Drawing.Size(216, 15);
+            this.lbTittle.TabIndex = 0;
+            this.lbTittle.Text = "Ingrese los datos del nuevo Cliente";
+            // 
+            // tbDNI
+            // 
+            this.tbDNI.Location = new System.Drawing.Point(90, 67);
+            this.tbDNI.MaxLength = 10;
+            this.tbDNI.Name = "tbDNI";
+            this.tbDNI.Size = new System.Drawing.Size(152, 20);
+            this.tbDNI.TabIndex = 1;
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(90, 102);
+            this.tbNombre.MaxLength = 150;
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(152, 20);
+            this.tbNombre.TabIndex = 2;
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Location = new System.Drawing.Point(90, 139);
+            this.tbTelefono.MaxLength = 20;
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(152, 20);
+            this.tbTelefono.TabIndex = 3;
+            // 
+            // lbDNI
+            // 
+            this.lbDNI.AutoSize = true;
+            this.lbDNI.Location = new System.Drawing.Point(29, 70);
+            this.lbDNI.Name = "lbDNI";
+            this.lbDNI.Size = new System.Drawing.Size(29, 13);
+            this.lbDNI.TabIndex = 4;
+            this.lbDNI.Text = "DNI:";
+            // 
+            // lbNombre
+            // 
+            this.lbNombre.AutoSize = true;
+            this.lbNombre.Location = new System.Drawing.Point(26, 105);
+            this.lbNombre.Name = "lbNombre";
+            this.lbNombre.Size = new System.Drawing.Size(47, 13);
+            this.lbNombre.TabIndex = 5;
+            this.lbNombre.Text = "Nombre:";
+            // 
+            // lbTelefono
+            // 
+            this.lbTelefono.AutoSize = true;
+            this.lbTelefono.Location = new System.Drawing.Point(26, 142);
+            this.lbTelefono.Name = "lbTelefono";
+            this.lbTelefono.Size = new System.Drawing.Size(52, 13);
+            this.lbTelefono.TabIndex = 6;
+            this.lbTelefono.Text = "Telefono:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(32, 182);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 7;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(167, 182);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // AgregarNuevoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 226);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.lbTelefono);
+            this.Controls.Add(this.lbNombre);
+            this.Controls.Add(this.lbDNI);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.tbNombre);
+            this.Controls.Add(this.tbDNI);
+            this.Controls.Add(this.lbTittle);
+            this.Name = "AgregarNuevoCliente";
+            this.Text = "AgregarNuevoCliente";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbTittle;
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.TextBox tbDNI;
+        private System.Windows.Forms.Label lbDNI;
+        private System.Windows.Forms.Label lbNombre;
+        private System.Windows.Forms.Label lbTelefono;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
